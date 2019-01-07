f7574af70ba58cf431680ae0590c1d7088a8d7cf
Ally <allison.avis@gmail.com>
2019-01-07 17:17:48 +0000

    pictures now appear on topics pages

---
+++ public/scripts/search.js
@@ -0,0 +1,16 @@
+$(() => {
+    $.ajax({
+      method: "GET",
+      url: "/api/searchresult"
+    }).done((resources) => {
+      for(details of resources) {
+        const $resource = $("<article>").addClass("resource").appendTo($("#resource"));
+        const $header = $("<header>").appendTo($resource);
+        $("<p class='resource-title'>").text(details.title).appendTo($header);
+        $("<p class='resource-description'>").text(details.description).appendTo($resource);
+        $("<p>").text(`Post by: ${details.username}`).appendTo($resource);
+        const $footer = $("<footer>").html(`<a href="/${details.id}">View resource</a>`).appendTo($resource);
+
+      }
+    });
+  });
